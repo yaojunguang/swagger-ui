842094bd09a29f70355a83973583b69d58390f90
yaojunguang <yaojunguang@outlook.com>
2020-09-06 01:39:16 +0800

    框架修改，采用vue-cli4

--- config/prod.env.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  ENV_CONFIG:'"prod"'
-}
--- src/main.js
+++ src/main.js
@@ -1,34 +1,45 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import VueResource from 'vue-resource'
-import VueClipboard from 'vue-clipboard2'
-import hljs from 'highlight.js'
-import 'highlight.js/styles/googlecode.css' //样式文件
+import Vue from 'vue';
+import App from './App.vue';
+import jquery from 'jquery';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueClipboard from 'vue-clipboard2';
+import highlight from 'highlight.js';
+import 'highlight.js/styles/googlecode.css'; //样式文件
 
 Vue.use(VueClipboard);
-Vue.use(VueResource);
 Vue.use(ElementUI);
-
 Vue.directive('highlight', function (el) {
-  let blocks = el.querySelectorAll('code');
-  setTimeout(() => {
-    blocks.forEach((block) => {
-      hljs.highlightBlock(block);
-    })
-  }, 200);
+    let blocks = el.querySelectorAll('code');
+    setTimeout(() => {
+        blocks.forEach((block) => {
+            highlight.highlightBlock(block);
+        })
+    }, 200);
 });
+
+window.jquery = window.$ = jquery;
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
+String.prototype.replaceAll = function (FindText, RepText) {
+    if (this != null) {
+        let regExp = new RegExp(FindText, 'g')
+        return this.replace(regExp, RepText)
+    }
+    return null
+};
+
+Array.prototype.contains = function (obj) {
+    let i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true
+        }
+    }
+    return false
+}
+
 new Vue({
-  el: '#app',
-  router,
-  components: {App},
-  template: '<App/>'
-});
+    render: h => h(App),
+}).$mount('#app');
