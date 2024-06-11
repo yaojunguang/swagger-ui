b223668f8673f4503a16d76e77fcf3e0a3dfa2f9
yaojunguang <yaojunguang@lattebank.com>
2024-06-11 20:13:44 +0800

    页面设置

--- ui/vite.config.mjs
+++ ui/vite.config.mjs
@@ -1,8 +1,7 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import viteCompression from 'vite-plugin-compression';
-
-const path = require('path');
+import path from 'path';
 
 export default defineConfig({
     //服务端渲染
